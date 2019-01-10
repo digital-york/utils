2ae090e3826030bd5fe9056f6d847d0f2a83da38
Frank Feng <y.frank.feng@gmail.com>
2019-01-10 11:32:30 +0000

    add code to check download statistics from both solr and fedora

--- iris_statistics_check/check.rb
+++ iris_statistics_check/check.rb
@@ -7,20 +7,12 @@ require 'nokogiri-pretty'
 
 class IrisStatisticsCheck
   def initialize()
-    @namespaces = { 'iris'   => 'http://iris-database.org/iris',
-                    'ds'     => 'http://www.fedora.info/definitions/1/0/access/',
-                    'dsp'    => 'http://www.fedora.info/definitions/1/0/management/'}
-
     puts 'loading system properties ...'
     @props    = PropertiesManager.new("system.yaml").getPropertiesHash()
     @protocol = @props['protocol']
     @host     = @props['host']
     @admin    = @props['admin']
     @password = @props['password']
-    @dumppath = @props['dumppath']
-    @dumpfile = @props['dumpfile']
-
-    @generatedfiles = Array.new
 
     puts 'loading pid list ...'
     @pids = []
@@ -33,7 +25,7 @@ class IrisStatisticsCheck
     f.close()
   end
 
-  def check()
+  def check_via_solr(show_progress_details=false)
     conn = Faraday.new(:url => @protocol + "://" + @host) do |c|
       c.use Faraday::Request::UrlEncoded
       #c.use Faraday::Response::Logger
@@ -43,96 +35,80 @@ class IrisStatisticsCheck
 
     total = @pids.length
     i = 1
+    total_downloads = 0
+
     for pid in @pids
-      puts 'Processing ' + pid + '['+i.to_s + ' / ' + total.to_s+']' +' ...'
+      if show_progress_details
+        puts '    Processing ' + pid + '['+i.to_s + ' / ' + total.to_s+']' +' ...'
+      else
+        print '.'
+      end
       pidforsolr = pid.dup
       pidforsolr.sub! ':', '?'
-      pidforfile = pid.dup
-      pidforfile.sub! ':', '_'
-      record = Nokogiri::XML('<record pid=\''+pid+'\'/>', nil, 'UTF-8')
-      recordRoot = record.root
 
-      puts '    Getting solr dump for ' + pid + ' ...'
       solrresponse = conn.get '/solr/select/?q=PID:'+ pidforsolr
       solrresponsedoc = solrresponse.body
 
-      if not solrresponse.nil?
-        # add solr xml response
-        solrnode = Nokogiri::XML::Node.new('solr',record)
-        recordRoot << solrnode
-        solrnode.add_child(solrresponsedoc)
-
-        # add associated files
-        puts '    getting uploaded files ...'
-        filesnode = Nokogiri::XML::Node.new('files',record)
-        recordRoot << filesnode
-
-        datastreamsresp = conn.get '/fedora/objects/'+pid+'/datastreams?format=xml'
-        datastreamsdoc  = Nokogiri::XML(datastreamsresp.body.to_s)
-        dss = datastreamsdoc.xpath("//ds:datastream[@label='INSTRUMENT']", @namespaces)
-        if not dss.nil?
-          for ds in dss
-            dsid   = ds['dsid']
-            dsmime = ds['mimeType']
-            dsurl  = 'https://www.iris-database.org/iris/api/resource/' + pid + '/asset/' + dsid + "?download=true"
-            filenode = Nokogiri::XML::Node.new('file',record)
-            filenode['id']   = dsid
-            filenode['mime'] = dsmime
-            filenode['url']  = dsurl
-            filesnode << filenode
-                        
-            #datastreamprofileresp = conn.get '/fedora/objects/'+pid+'/datastreams/'+dsid+'?format=xml'
-            #datastreamprofiledoc  = Nokogiri::XML(datastreamprofileresp.body.to_s)
-
-            #dslocation_elts = datastreamprofiledoc.xpath('/dsp:datastreamProfile/dsp:dsLocation', @namespaces)
-            #if not dslocation_elts.nil?
-            #  for dslocation_elt in dslocation_elts
-            #    dslocation = dslocation_elt.text
-            #    filenode = Nokogiri::XML::Node.new('file',record)
-            #    filenode['id'] = dsid
-            #    filenode['mime'] = dsmime
-            #    filenode['url']  = dslocation
-            #    filesnode << filenode
-            #  end
-            #end
-          end
-        else
-          puts "    Cannot find any uploaded file for " + pid
-        end
+      unless solrresponsedoc.nil?
+        solrdoc  = Nokogiri::XML(solrresponsedoc.to_s)
+        dss = solrdoc.xpath("/response/result/doc/arr[@name='iris.downloaders.uniquecount']/str/text()")
+        total_downloads = total_downloads + dss.to_s.to_i
       else
-        puts 'Error: cannot dump solr data!'
+        puts 'Error: cannot search in solr!'
       end
-      record.search("//arr[starts-with(@name,'iris.downloaders') or starts-with(@name,'iris2.downloaders')]").remove
-      record.search("//arr[starts-with(@name,'vra.') or starts-with(@name,'vra2.')]").remove
-      record.search("//arr[starts-with(@name,'acl.') or starts-with(@name,'acl2.')]").remove
-      record.search("//arr[starts-with(@name,'rdf.') or starts-with(@name,'rdf2.')]").remove
 
-      puts '    Saving to file ' + @dumppath + pidforfile + '.xml'
-      File.write(@dumppath + pidforfile + '.xml', record.human)
-      @generatedfiles << (pidforfile + '.xml')
       i = i+1
-      if i%50 == 0
-        puts 'Sleeping for 5s ...'
-        sleep(5)
+
+      if i%100==0
+        sleep(1)
       end
     end
 
-    puts 'Creating zip file ...'
-    zipfilename = @dumpfile
-    if File.exist? zipfilename
-      File.delete(zipfilename)
-    end
-    Zip::File.open(zipfilename, Zip::File::CREATE) do |zipfile|
-      for f in @generatedfiles
-        zipfile.add(f, @dumppath + f)
-      end
-    end
+    puts "From Solr: #{total_downloads}"
 
   end
+
+  def check_via_fedora(show_progress_details=false)
+    conn = Faraday.new(:url => @protocol + "://" + @host) do |c|
+      c.use Faraday::Request::UrlEncoded
+      c.use Faraday::Adapter::NetHttp
+    end
+    conn.basic_auth(@admin, @password)
+
+    total = @pids.length
+    i = 1
+    total_downloads = 0
+
+    @pids.each do |p|
+      if show_progress_details
+        puts "Checking #{p.to_s.strip}: [#{i.to_s} / #{total.to_s}] ..."
+      else
+        print '.'
+      end
+      downloader_url = "/fedora/objects/#{p.to_s.strip}/datastreams/DOWNLOADER/content"
+
+      downloader       = conn.get downloader_url
+      downloader_doc   = Nokogiri::XML(downloader.body.to_s)
+      downloader_count = downloader_doc.xpath("count(/downloaders/downloader[@duplicate='false'])")
+      #puts downloader_count.to_s.to_i
+      total_downloads = total_downloads + downloader_count.to_s.to_i
+
+      i = i+1
+
+      if i%100==0
+        sleep(1)
+      end
+    end
+
+    puts "From Fedora: #{total_downloads}"
+
+  end
+
 end
 
 isc = IrisStatisticsCheck.new()
-isc.check()
+isc.check_via_solr()
+#isc.check_via_fedora()
 
 
 
